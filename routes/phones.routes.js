bb365a4a1d67a668846f6fc9edb7ae83992d5e88
Wilkins Morales <wilkinscom@gmail.com>
2023-06-10 10:56:23 +0200

    get all phones ok

--- routes/phones.routes.js
+++ routes/phones.routes.js
@@ -1,9 +1,19 @@
+const fs = require("fs");
+const path = require("path");
 const router = require("express").Router();
 
-router.get("/phones", (req, res, next) => {
-  res.json(
-    "here we will have all the phones listed inside the data/phones.json file"
-  );
+router.get("/phones", (req, res) => {
+  const filePath = path.join(__dirname, "../data/phones.json");
+
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("Error reading phones data.");
+    }
+
+    const phones = JSON.parse(data);
+    res.json(phones);
+  });
 });
 
 router.get("/phones/:id", (req, res, next) => {
