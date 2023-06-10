50dc5cb77a49baddc98564974eacd6e48b74c43a
Wilkins Morales <wilkinscom@gmail.com>
2023-06-10 11:14:55 +0200

    phoneData extracted GET routes ok

--- routes/phones.routes.js
+++ routes/phones.routes.js
@@ -2,22 +2,25 @@ const fs = require("fs");
 const path = require("path");
 const router = require("express").Router();
 
+// Make phone Data available for the routes
+const filePath = path.join(__dirname, "../data/phones.json");
+const phonesData = fs.readFileSync(filePath, "utf8");
+const phones = JSON.parse(phonesData);
+
 router.get("/phones", (req, res) => {
-  const filePath = path.join(__dirname, "../data/phones.json");
-
-  fs.readFile(filePath, "utf8", (err, data) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).send("Error reading phones data.");
-    }
-
-    const phones = JSON.parse(data);
-    res.json(phones);
-  });
+  res.json(phones);
 });
 
-router.get("/phones/:id", (req, res, next) => {
-  res.json("here we will have the details for an individual phone");
+router.get("/phones/:id", (req, res) => {
+  const phoneId = req.params.id;
+
+  const phone = phones.find((p) => p.id == phoneId);
+
+  if (!phone) {
+    return res.status(404).send("Phone not found");
+  }
+
+  res.json(phone);
 });
 
 module.exports = router;
